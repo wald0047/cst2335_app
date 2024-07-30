7283cdc99a8c3cf76a523b9503f1d63f80b73b8a
wald0047 <163198863+wald0047@users.noreply.github.com>
2024-07-30 17:24:41 -0400

    Cleanup airplane page.

--- lib/airplanepage.dart
+++ lib/airplanepage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cst2335_app/airplane.dart';
 import 'package:cst2335_app/airplanedao.dart';
@@ -7,12 +8,14 @@ import 'database.dart';
 
 class AirplanePage extends StatefulWidget {
   const AirplanePage({super.key});
+
   @override
   State<AirplanePage> createState() => AirplanePageState();
 }
 
 class AirplanePageState extends State<AirplanePage> {
   List<Airplane> planes = [];
+  late TextEditingController idTextCtl;
   late TextEditingController typeTextCtl;
   late TextEditingController passengerTextCtl;
   late TextEditingController rangeTextCtl;
@@ -25,6 +28,7 @@ class AirplanePageState extends State<AirplanePage> {
   void initState() {
     // initialize object, onloaded in HTML
     super.initState();
+    idTextCtl = TextEditingController();
     typeTextCtl = TextEditingController();
     passengerTextCtl = TextEditingController();
     rangeTextCtl = TextEditingController();
@@ -57,6 +61,7 @@ class AirplanePageState extends State<AirplanePage> {
     DataRepository.planeRange = rangeTextCtl.value.text;
     DataRepository.planeSpeed = speedTextCtl.value.text;
     DataRepository.saveData();
+    idTextCtl.dispose();
     typeTextCtl.dispose();
     passengerTextCtl.dispose();
     rangeTextCtl.dispose();
@@ -84,7 +89,7 @@ class AirplanePageState extends State<AirplanePage> {
       return Row(
         children: [
           Expanded(flex: 2, child: Center(child: AirplaneList())),
-          Expanded(flex: 3, child: Column(children: [DetailsPage(false)])),
+          Expanded(flex: 3, child: Column(children: [DetailsPage()])),
         ],
       );
     } else {
@@ -92,48 +97,58 @@ class AirplanePageState extends State<AirplanePage> {
       if (selectedPlane == null) {
         return AirplaneList();
       } else {
-        return DetailsPage(true);
+        return DetailsPage();
       }
     }
   }
 
   Widget AirplaneList() {
-    return Center(
-      child: Column(
-        children: [
-          EditWidgets(),
-          ElevatedButton(
-              onPressed: addPlane,
-              child: Text("Add")),
-          Expanded(
-            child: ListView.builder(
-              itemCount: planes.length,
-              itemBuilder: (context, rowNum) {
-                // return the widget to go on row : rowNum
-                return GestureDetector(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Row(children: [Text(textAlign: TextAlign.left, "Planes:")]),
+        ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: planes.length,
+          itemBuilder: (context, rowNum) {
+            // return the widget to go on row : rowNum
+            return Card(
+                child: InkWell(
                     onTap: () {
-                       setState(() {
-                         selectedPlane = planes[rowNum];
-                         typeTextCtl.text = selectedPlane!.type;
-                         passengerTextCtl.text = selectedPlane!.numPassengers.toString();
-                         speedTextCtl.text = selectedPlane!.maxSpeed.toString();
-                         rangeTextCtl.text = selectedPlane!.range.toString();
+                      setState(() {
+                        selectedPlane = planes[rowNum];
+                        idTextCtl.text = selectedPlane!.id.toString();
+                        typeTextCtl.text = selectedPlane!.type;
+                        passengerTextCtl.text =
+                            selectedPlane!.numPassengers.toString();
+                        speedTextCtl.text = selectedPlane!.maxSpeed.toString();
+                        rangeTextCtl.text = selectedPlane!.range.toString();
                       });
                     },
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("id: ${planes[rowNum].id ?? ""}"),
-                          Text(planes[rowNum].type),
-                          Text(planes[rowNum].range.toString()),
-                          Text(planes[rowNum].maxSpeed.toString()),
-                          Text(planes[rowNum].numPassengers.toString())
-                        ]));
-              },
-            ),
+                    child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Row(children: [
+                          Icon(CupertinoIcons.airplane, color: Colors.black),
+                          Text(planes[rowNum].type)
+                        ]))));
+          },
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              selectedPlane = Airplane.noid('', -1, -1, -1);
+            });
+          },
+          child: Icon(Icons.add, color: Colors.white),
+          style: ElevatedButton.styleFrom(
+            shape: CircleBorder(),
+            padding: EdgeInsets.all(20),
+            backgroundColor: Colors.blue, // <-- Button color
+            foregroundColor: Colors.red, // <-- Splash color
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 
@@ -148,7 +163,7 @@ class AirplanePageState extends State<AirplanePage> {
         builder: (BuildContext context) => AlertDialog(
           title: const Text('Confirm Airplane Removal'),
           content: Text('Are you sure you would like to remove:\n' +
-          'id:${planes[index].id}\t${planes[index].type}'),
+              '${planes[index].type}'),
           actions: <Widget>[
             TextButton(
               child: const Text('Yes'),
@@ -176,133 +191,132 @@ class AirplanePageState extends State<AirplanePage> {
     });
   }
 
-  Widget DetailsPage(bool editWidgets) {
+  Widget DetailsPage() {
     if (selectedPlane == null) {
       return Text("Nothing is selected");
     } else {
       Airplane plane = selectedPlane!;
-      var widgets = [
-        Text("id: ${plane.id ?? ""}"),
-        Row(mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-            children:[
-              ElevatedButton(
+      var edit = [
+        PlaneForm(),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Padding(
+              padding: EdgeInsets.all(4),
+              child: ElevatedButton(
                   onPressed: () {
                     setState(() {
                       selectedPlane = null;
                     });
                   },
-                  child: Text("Unselect")),
-              ElevatedButton(
+                  child: Text("Unselect"))),
+          Padding(
+              padding: EdgeInsets.all(4),
+              child: ElevatedButton(
                   onPressed: () {
                     int index = planes.indexOf(selectedPlane!);
-                    Airplane newPlane = Airplane(selectedPlane?.id,
+                    Airplane newPlane = Airplane(
+                        selectedPlane?.id,
                         typeTextCtl.value.text,
                         int.parse(passengerTextCtl.value.text),
                         int.parse(speedTextCtl.value.text),
-                        int.parse(rangeTextCtl.value.text)
-                    );
+                        int.parse(rangeTextCtl.value.text));
                     airplaneDAO.updateAirplane(newPlane);
                     setState(() {
                       planes[index] = newPlane;
                       selectedPlane = newPlane;
                     });
                   },
-                  child: Text("Update")),
-              ElevatedButton(
+                  child: Text("Update"))),
+          Padding(
+              padding: EdgeInsets.all(4),
+              child: ElevatedButton(
                   onPressed: () {
                     deleteAirplane(plane);
                   },
-                  child: Text("Delete"))])
+                  child: Text("Delete"))),
+        ]),
       ];
-      if (editWidgets) {
-        widgets.insert(1, EditWidgets());
+      var add = [
+        PlaneForm(),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          ElevatedButton(
+              onPressed: () {
+                addPlane();
+              },
+              child: Text("Add")),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  selectedPlane = null;
+                });
+              },
+              child: Text("Clear")),
+        ])
+      ];
+      if (selectedPlane!.id == null) {
+        return Column(children: add);
+      } else {
+        return Column(children: edit);
       }
-      return Column(children: widgets);
     }
   }
 
-  Widget EditWidgets() {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text("Type"),
-            Expanded(
-              child:
-              TextField(
-                controller:typeTextCtl,
-                decoration: const InputDecoration(
-                  hintText: 'Type',
-                ),
+  Widget PlaneForm() {
+    return Form(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+          selectedPlane?.id != null
+              ? TextFormField(
+                  controller: idTextCtl,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.numbers),
+                    border: OutlineInputBorder(),
+                  ),
+                  enabled: false,
+                )
+              : Text('New Plane:'),
+          TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(CupertinoIcons.airplane),
+                hintText: 'What is the plane called?',
+                labelText: 'Plane\'s name',
               ),
-            )
-          ]
-      ),
-        Row(
-            children: [
-              Text("Passengers"),
-              Expanded(
-                child:
-                TextField(
-                  controller:passengerTextCtl,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  keyboardType: const TextInputType.numberWithOptions(
-                      decimal: false,
-                      signed: false
-                  ),
-                  decoration: const InputDecoration(
-                    hintText: 'Passengers',
-                  ),
-                ),
-              )
-            ]
-        ),
-        Row(
-            children: [
-              Text("Range"),
-              Expanded(
-                child:
-                TextField(
-                  controller:rangeTextCtl,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  keyboardType: const TextInputType.numberWithOptions(
-                      decimal: false,
-                      signed: false
-                  ),
-                  decoration: const InputDecoration(
-                    hintText: 'Range in km',
-                  ),
-                ),
-              )
-            ]
-        ),
-        Row(
-            children: [
-              Text("Speed"),
-              Expanded(
-                child:
-                TextField(
-                  controller:speedTextCtl,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  keyboardType: const TextInputType.numberWithOptions(
-                      decimal: false,
-                      signed: false
-                  ),
-                  decoration: const InputDecoration(
-                    hintText: 'Speed in km/h',
-                  ),
-                ),
-              )
-            ]
-        ),
-    ],);
+              controller: typeTextCtl),
+          TextFormField(
+            controller: passengerTextCtl,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            keyboardType: const TextInputType.numberWithOptions(
+                decimal: false, signed: false),
+            decoration: const InputDecoration(
+              icon: Icon(CupertinoIcons.person),
+              hintText: 'What is the number of passengers?',
+              labelText: 'Number of passengers',
+            ),
+          ),
+          TextFormField(
+            controller: rangeTextCtl,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            keyboardType: const TextInputType.numberWithOptions(
+                decimal: false, signed: false),
+            decoration: const InputDecoration(
+                icon: Icon(Icons.route),
+                hintText: 'What is the range of the plane?',
+                labelText: 'Range (km)'),
+          ),
+          TextFormField(
+            controller: speedTextCtl,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            keyboardType: const TextInputType.numberWithOptions(
+                decimal: false, signed: false),
+            decoration: const InputDecoration(
+                icon: Icon(Icons.speed),
+                labelText: 'Speed (km/h)',
+                hintText: 'What is the speed of the plane?'),
+          ),
+        ]));
   }
+
   //This function gets run when you click the button
   void addPlane() {
     int idx = planes.length;
@@ -310,10 +324,10 @@ class AirplanePageState extends State<AirplanePage> {
         typeTextCtl.value.text,
         int.parse(passengerTextCtl.value.text),
         int.parse(speedTextCtl.value.text),
-        int.parse(rangeTextCtl.value.text)
-    );
+        int.parse(rangeTextCtl.value.text));
     setState(() {
       idx = planes.length;
+      selectedPlane = noid;
       planes.add(noid);
       typeTextCtl.text = "";
       passengerTextCtl.text = "";
@@ -323,6 +337,7 @@ class AirplanePageState extends State<AirplanePage> {
     airplaneDAO.insertAirplane(noid).then((id) {
       setState(() {
         planes[idx] = Airplane.fromNoid(id, noid);
+        selectedPlane = planes[idx];
       });
     });
   }
